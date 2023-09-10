af8561714bc38518602ced2e189adc11c0e8c0eb
v.mezentsev <v.mezentsev@air-burg.ru>
2023-09-10 18:39:37 +0500

    добавлены проекты для библиотеки modm добавлена поддержка cmake для сборки примеров с библиотекой modm

---
+++ atmega/modm-avr-timeout/main.cpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2010-2011, Fabian Greif
+ * Copyright (c) 2012-2013, 2015-2017, Niklas Hauser
+ * Copyright (c) 2014, Sascha Schade
+ *
+ * This file is part of the modm project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+// ----------------------------------------------------------------------------
+
+#include <modm/platform.hpp>
+#include <modm/processing/timer.hpp>
+
+using namespace modm::platform;
+using namespace std::chrono_literals;
+
+// create a output device for the led
+typedef GpioOutputB5 Led;
+
+
+int main()
+{
+    SystemClock::enable();
+    Led::setOutput();
+    Led::reset();
+
+    // enable interrupts
+    enableInterrupts();
+
+    modm::ShortTimeout timeout( 200ms );
+
+    while ( true )
+    {
+        if ( timeout.isExpired() )
+        {
+            timeout.restart( 200ms );
+
+            Led::toggle();
+        }
+    }
+}
